5eaae68c9634811e04c41550180c5c369411898e
rajasekharsourcebits <54577320+rajasekharsourcebits@users.noreply.github.com>
2020-05-19 19:49:42 +0530

    Add files via upload

---
+++ SwiftUI-POC/SwiftUI-POC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-POC
+//
+//  Created by Rajshekar on 19/05/20.
+//  Copyright © 2020 Sourcebits. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
